eed2426ba22ca9a68dbc648b50838b9d8c53a9cc
rinne13 <varvararinne@gmail.com>
2024-12-04 12:07:51 +0200

    origin main

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/02-part2-react-kurssitiedot", 
+base: "rinne13-02-part2-react-kurssitiedot",
   plugins: [react()],
 })
